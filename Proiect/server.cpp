de24ecce06ffd8931a151bb3c8f2d88efa313e07
maxcox <radudan610@gmail.com>
2024-12-01 18:22:16 +0200

    Updated the server with socket listening and accepting functionality. (synchronous for now)

--- Proiect/server.cpp
+++ Proiect/server.cpp
@@ -8,6 +8,9 @@
 #include<cstring>
 #include <string>
 #include <cerrno>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <unistd.h>
 using namespace std;
 
 void create_socket(int& serversocket) {
@@ -37,9 +40,36 @@ void bind_socket(int& serversocket, int port) {
     else cout<<"bind() OK"<<endl;
 }
 
+// listen(socket, int backlog); backlog=limita de clienti pt socket
+
+void socket_listens(int& serversocket) {
+    if (listen(serversocket,10)==-1) {  //am ales 10 clienti reprezentand un numar de 10 etaje teoretice posibile ale unei parcari
+        cerr<<"listen():Error listening to socket "<< strerror(errno)<<endl;
+        close(serversocket);
+        exit(1);
+    }
+    else cout<<"listen() OK, waitig for connections..."<<endl;
+}
+
+//accept(socket, struct sockaddr* addr, int* addrlen)  sockaddr* addr este adresa clientului, e folositor daca vrei sa accepti informatii doar de la un client anume.
+                                                     //int* addrlen size of the address structure ^^
+//RETURNS A VALUE OF TYPE INT!! mai exact un ALT SOCKET, o DUBLURA cu acelasi PORT si IP_adress cu care comunica explicit cu clientul desemnat.
+
+void accept_socket(int& serversocket, int& acceptsocket) {
+    acceptsocket=accept(serversocket, NULL, NULL);
+    if (acceptsocket==-1) {
+        cerr<<"accept() failed: "<< strerror(errno)<<endl;
+        exit(1);
+    }
+    else cout<<"se accepta()"<<endl;
+}
+
 int main() {
     int socketfd=-1;
     create_socket(socketfd);
     bind_socket(socketfd, 55555);
+    socket_listens(socketfd);
+    int acceptsocketfd=-1;
+    accept_socket(socketfd,acceptsocketfd);
     return 0;
 }
