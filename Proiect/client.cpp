0d128e46cddb5976f0f6ad650c6ae275090cb72c
maxcox <radudan610@gmail.com>
2024-12-06 21:56:08 +0200

    IMPLEMENTED CONCURRENCY!!! But code needs refining, although it is functional and relatively easy to read.

--- Proiect/client.cpp
+++ Proiect/client.cpp
@@ -15,6 +15,7 @@
 #include <sys/select.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <sys/epoll.h>
 using namespace std;
 
 void create_socket(int& clientsocket) {
@@ -40,14 +41,7 @@ void connect_socks(int &clientsocket, struct sockaddr_in &clientService) {
     cout<<"Client: Can start sending and receiving data..."<<endl;
 }
 
-// void receive_data(int &clientsocket, struct sockaddr_in &clientService) {
-//   char buffer[1024];
-//   int bytes_received=recv(clientsocket, buffer, sizeof(buffer), 0);
-//   if (bytes_received==-1) {
-//     cerr<<"Error with the connection(): "<< strerror(errno)<<endl;
-//     //close(clientsocket);
-//   }
-// }
+
 void send_data(int &clientsocket, const string &mesaj) {
     const char* buffer = mesaj.c_str();
     size_t buffer_len = mesaj.length();
@@ -58,15 +52,105 @@ void send_data(int &clientsocket, const string &mesaj) {
     else cout<<"Client: sent "<< bytes_sent << " bytes"<<endl;
 }
 
+void receive_data(int &clientsocket, string& message) {
+  char buffer[200];
+  int bytes_received=recv(clientsocket, buffer, sizeof(buffer), 0);
+  if (bytes_received<0) {
+        cerr<<"Error with the connection(): "<< strerror(errno)<<endl;
+        exit(1);
+  }
+  else if (bytes_received==0) {
+      cout<<"Server disconnected."<<endl;
+      close(clientsocket);
+      exit(1);
+  }
+  else{
+      buffer[bytes_received]='\0';
+      message = buffer;
+      cout<<"received data: "<<message<<endl;
+  }
+}
+
+void handle_user_input(int clientsocket) {
+    string message;
+    cout<<"Enter message to send: "<<endl;
+    getline(cin, message);
+    send_data(clientsocket, message);
+    if (message=="pa") {
+        cout<<"Client requested to end the chat"<<endl;
+        close(clientsocket);
+        exit(0);
+    }
+}
+
+int setup_epoll(int clientsocket) {
+    int epollfd = epoll_create1(0);
+    if (epollfd==-1) {
+        cerr<<"epoll_create1() failed: "<< strerror(errno)<<endl;
+        exit(1);
+    }
+    struct epoll_event ev;
+    ev.events = EPOLLIN | EPOLLOUT;
+    ev.data.fd = clientsocket;
+    if (epoll_ctl(epollfd,EPOLL_CTL_ADD, clientsocket, &ev)==-1) {
+        cerr<<"epoll_ctl() failed: "<<strerror(errno)<<endl;
+        close(epollfd);
+        exit(1);
+    }
+    return epollfd;
+}
+
+void handle_server_data(int clientsocket) {
+    string message;
+    receive_data(clientsocket, message);
+    if (message=="pa") {
+        cout<<"Server requested to end the chat."<<endl;
+        close(clientsocket);
+        exit(0);
+    }
+}
 
 int main() {
     int client_socket=-1;
     create_socket(client_socket);
     struct sockaddr_in client_service;
     connect_socks(client_socket, client_service);
-    string message;
-    cout<<"Enter message to send..."<<endl;
-    getline(cin, message);
-    send_data(acceptsocketfd,message);
+    // string message;
+    // string mesaj_primit;
+    // do {
+    //     cout << "Enter message to send: " << endl;
+    //     getline(cin, message);
+    //     send_data(client_socket, message);
+    //     if (message == "pa") {
+    //         cout << "Client requested to end the chat." << endl;
+    //         break;
+    //     }
+    //                                                                         implementarea asta era pentru un server sincron, nu concurent
+    //     receive_data(client_socket, client_service, mesaj_primit);
+    //     cout << "Received message: " << mesaj_primit << endl;
+    //     if (mesaj_primit == "pa") {
+    //         cout << "Server requested to end the chat." << endl;
+    //         break;
+    //     }
+    // } while (true);
+    // close(client_socket);
+    int epollfd =setup_epoll(client_socket);
+    const int MAX_EVENTS=1;
+    struct epoll_event events[MAX_EVENTS];
+    while (true) {
+        int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+        if (nfds==-1) {
+            cerr<<"epoll_wait() failed: "<<strerror(errno)<<endl;
+            close(epollfd);
+            close(client_socket);
+            exit(1);
+        }
+        if (nfds>0) {
+            handle_user_input(client_socket);
+            handle_server_data(client_socket);
+        }
+    }
+    close(epollfd);
+    close(client_socket);
     return 0;
 }
--- Proiect/server.cpp
+++ Proiect/server.cpp
@@ -12,7 +12,10 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <string>
+#include <sys/epoll.h>
 using namespace std;
+
+const int MAX_ETAJE = 10;
 
 void create_socket(int& serversocket) {
     serversocket=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -44,7 +47,7 @@ void bind_socket(int& serversocket, int port) {
 // listen(socket, int backlog); backlog=limita de clienti pt socket
 
 void socket_listens(int& serversocket) {
-    if (listen(serversocket,10)==-1) {  //am ales 10 clienti reprezentand un numar de 10 etaje teoretice posibile ale unei parcari
+    if (listen(serversocket,MAX_ETAJE)==-1) {  //am ales 10 clienti reprezentand un numar de 10 etaje teoretice posibile ale unei parcari
         cerr<<"listen():Error listening to socket "<< strerror(errno)<<endl;
         close(serversocket);
         exit(1);
@@ -67,37 +70,135 @@ void accept_socket(int& serversocket, int& acceptsocket) {
     else cout<<"se accepta()"<<endl;
 }
 
-// void send_data(int &acceptsocket, const string &mesaj) {
-//     const char* buffer = mesaj.c_str();
-//     size_t buffer_len = mesaj.length();
-//     int bytes_sent= send(acceptsocket, buffer, buffer_len, 0);
-//     if (bytes_sent ==-1) {
-//         cerr<<"send() failed: "<< strerror(errno)<<endl;
-//     }
-//     else cout<<"Server: sent "<< bytes_sent << " bytes"<<endl;
-// }
-
-void receive_data(int &acceptsocket, struct sockaddr_in &serverService) {
+void receive_data(int &acceptsocket, struct sockaddr_in &serverService, string& message) {
     char buffer[200];
     int bytes_received = recv(acceptsocket, buffer, sizeof(buffer), 0);
-    if (bytes_received==-1) {
+    if (bytes_received<0) {
         cerr<<"Error with the connection(): "<< strerror(errno)<<endl;
-        //close(clientsocket);
+        exit(1);
+    }
+    else if (bytes_received==0) {
+        cout<<"client disconnected"<<endl;
+        close(acceptsocket);
+    }
+    else{
+        buffer[bytes_received]='\0';
+        message = buffer;
+        cout<<"received data: "<<message<<endl;
     }
 }
 
-int main() {
-    int socketfd=-1;
-    create_socket(socketfd);
-    bind_socket(socketfd, 55555);
-    socket_listens(socketfd);
-    int acceptsocketfd=-1;
-    accept_socket(socketfd,acceptsocketfd);
+void send_data(int &acceptsocket, const string &mesaj) {
+    const char* buffer = mesaj.c_str();
+    size_t buffer_len = mesaj.length();
+    int bytes_sent= send(acceptsocket, buffer, buffer_len, 0);
+    if (bytes_sent ==-1) {
+        cerr<<"send() failed: "<< strerror(errno)<<endl;
+    }
+    else cout<<"Server: sent "<< bytes_sent << " bytes"<<endl;
+}
+
+void handle_client(int epollfd, int serversocket) {
+    struct epoll_event ev;
+    int clientsocket=accept(serversocket, NULL, NULL);
+    if (clientsocket==-1) {
+        cerr<<"accept() failed: "<<strerror(errno)<<endl;
+        return;
+    }
+    ev.events=EPOLLIN;
+    ev.data.fd=clientsocket;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, clientsocket, &ev)==-1) {
+        cerr<<"epoll_ctl() failed"<<strerror(errno)<<endl;
+        close(clientsocket);
+        return;
+    }
+    cout<<"New client connected"<<endl;
+}
+
+void handle_communication(int epollfd, int clientsocket) {
     struct sockaddr_in server_service;
-    receive_data(acceptsocketfd, server_service);
-//    string message;
-//    cout<<"Enter message to send"<<endl;
-//    getline(cin, message);
-//    send_data(acceptsocketfd,message);
+    string message;
+    receive_data(clientsocket, server_service, message);
+    if (message=="pa") {
+        cout<<"client requested to end the chat."<<endl;
+        close(clientsocket);
+        exit(0);
+    }
+    cout<<"enter message to be sent: "<<endl;
+    getline(cin, message);
+    send_data(clientsocket, message);
+    if (message=="pa") {
+        cout<<"Server requested to end the chat."<<endl;
+        close(clientsocket);
+        exit(0);
+    }
+}
+
+int setup_epoll(int serversocket) {
+    int epollfd=epoll_create1(0);
+    if (epollfd==-1) {
+        cerr<<"epoll_create1() failed "<< strerror(errno)<<endl;
+        exit(1);
+    }
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd=serversocket;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, serversocket, &ev)==-1) {
+        cerr<<"epoll_ctl() failed: "<<strerror(errno)<<endl;
+        close(epollfd);
+        exit(1);
+    }
+    return epollfd;
+}
+
+
+int main() {
+    int serversocketfd=-1;
+    create_socket(serversocketfd);
+    bind_socket(serversocketfd, 55555);
+    socket_listens(serversocketfd);
+    // int acceptsocketfd=-1;
+    // accept_socket(socketfd,acceptsocketfd);
+    // struct sockaddr_in server_service;
+    // string mesaj_primit;
+    // string mesaj;
+    // do {
+    //     receive_data(acceptsocketfd, server_service, mesaj_primit);
+    //     cout << "Received message: " << mesaj_primit << endl;
+    //     if (mesaj_primit == "pa") {
+    //         cout << "Client requested to end the chat." << endl;
+    //         break;
+    //     }
+    //                                                                              implementare facuta pt un server sincron, nu asincron
+    //     cout << "Enter message to send: " << endl;
+    //     getline(cin, mesaj);
+    //     send_data(acceptsocketfd, mesaj);
+    //     if (mesaj == "pa") {
+    //         cout << "Server requested to end the chat." << endl;
+    //         break;
+    //     }
+    // } while (true);
+    // close(acceptsocketfd);
+    // close(socketfd);
+    int epollfd=setup_epoll(serversocketfd);
+
+    struct epoll_event etaje[MAX_ETAJE];
+    while (true) {
+        int nfds=epoll_wait(epollfd, etaje, MAX_ETAJE, -1);
+        if (nfds==-1) {
+            cerr<<"epoll_wait() failed: "<<strerror(errno)<<endl;
+            close(epollfd);
+            close(serversocketfd);
+            exit(1);
+        }
+        for (int i=0; i<nfds; i++) {
+            if (etaje[i].data.fd==serversocketfd) {
+                handle_client(epollfd, serversocketfd);
+            }
+            else handle_communication(epollfd, etaje[i].data.fd);
+        }
+    }
+    close(epollfd);
+    close(serversocketfd);
     return 0;
 }
